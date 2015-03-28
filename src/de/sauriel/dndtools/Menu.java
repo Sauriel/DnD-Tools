fd450ecf698f529ca4207034f4ee3810b0bc7c58
Sascha Drux <s_drux@gmx.de>
2015-03-28 17:02:33 +0100

    adds encounter calculation

--- src/de/sauriel/dndtools/Menu.java
+++ src/de/sauriel/dndtools/Menu.java
@@ -1,10 +1,12 @@
 package de.sauriel.dndtools;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import de.sauriel.dndtools.buildings.Building;
 import de.sauriel.dndtools.buildings.BuildingCreator;
+import de.sauriel.dndtools.encounter.EncounterXpCalculation;
 import de.sauriel.dndtools.settlements.Settlement;
 import de.sauriel.dndtools.settlements.SettlementCreator;
 
@@ -13,6 +15,7 @@ public class Menu {
 	private Scanner scanner = new Scanner(System.in);
 	private BuildingCreator buildingCreator = new BuildingCreator();
 	private SettlementCreator settlementCreator = new SettlementCreator(buildingCreator);
+	private EncounterXpCalculation encounterCalculation = new EncounterXpCalculation();
 
 	public void showMenu() {
 		while (true) {
@@ -20,6 +23,7 @@ public class Menu {
 			System.out.println("# 0.\tExit Program");
 			System.out.println("# 1.\tCreate Settlement");
 			System.out.println("# 2.\tCreate Building");
+			System.out.println("# 3.\tCalculate Encounter");
 			System.out.println("### Choose your action ###");
 			try {
 				selectAction();
@@ -44,6 +48,9 @@ public class Menu {
 		case "2":
 			createBuilding();
 			break;
+		case "3":
+			calculateEncounter();
+			break;
 		default:
 			throw new InputMismatchException();
 		}
@@ -55,24 +62,70 @@ public class Menu {
 	}
 	
 	private void createSettlement() {
-		System.out.print("Please enter a name for the Settlement: ");
+		System.out.print("Please enter a name for the settlement: ");
 		String settlementName = scanner.next();
-		System.out.print("How many specific buildings would you like to create for the Settlement? ");
-		int numberOfBuildings = 0;
+		System.out.print("How many specific buildings would you like to create for the settlement? ");
 		try {
-			numberOfBuildings = scanner.nextInt();
+			int numberOfBuildings = scanner.nextInt();
+			Settlement town = settlementCreator.create(settlementName, numberOfBuildings);
+			blankLine();
+			System.out.println(town);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Invalid input.");
 		}
-		Settlement town = settlementCreator.create(settlementName, numberOfBuildings);
-		blankLine();
-		System.out.println(town);
 	}
 	
 	private void createBuilding() {
 		Building building = buildingCreator.create();
 		System.out.println(building);
+	}
+	
+	private void calculateEncounter() {
+		System.out.print("How many players are in the party? ");
+		try {
+			int partySize = scanner.nextInt();
+			if (partySize < 1) {
+				throw new Exception();
+			}
+			
+			ArrayList<Integer> partyLevel = new ArrayList<>();
+			while (partyLevel.size() < partySize) {
+				System.out.print("What level has Player " + (partyLevel.size() + 1) + "? ");
+				try {
+					int level = scanner.nextInt();
+					partyLevel.add(Integer.valueOf(level));
+				} catch (Exception e) {
+					System.out.println("Invalid input.");
+				}
+			}
+			
+			System.out.print("How many monsters do you want? ");
+			int numberOfMonsters = scanner.nextInt();
+			
+			System.out.print("What difficulty do you want? (Easy, Medium, Hard, Deadly) ");
+			String difficultySelect = scanner.next();
+			int[] difficulty;
+			switch (difficultySelect.toLowerCase().charAt(0)) {
+			case 'd':
+				difficulty = EncounterXpCalculation.DEADLY_XP;
+				break;
+			case 'e':
+				difficulty = EncounterXpCalculation.EASY_XP;
+				break;
+			case 'h':
+				difficulty = EncounterXpCalculation.HARD_XP;
+				break;
+			case 'm':
+			default:
+				difficulty = EncounterXpCalculation.MEDIUM_XP;
+				break;
+			}
+			
+			int calculatedXP = encounterCalculation.calculateXP(partyLevel, numberOfMonsters, difficulty);
+			System.out.println("You can use: " + calculatedXP + " XP");
+		} catch (Exception e) {
+			System.out.println("Invalid input.");
+		}
 	}
 
 }
---
+++ src/de/sauriel/dndtools/encounter/EncounterXpCalculation.java
@@ -0,0 +1,53 @@
+package de.sauriel.dndtools.encounter;
+
+import java.util.ArrayList;
+
+import de.sauriel.dndtools.tools.RangedTreeMap;
+
+public class EncounterXpCalculation {
+	
+	public static final int[] EASY_XP = {25,50,75,125,250,300,350,450,550,600,800,1000,1100,1250,1400,1600,2000,2100,2400,2800};
+	public static final int[] MEDIUM_XP = {50,100,150,250,500,600,750,900,1100,1200,1600,2000,2200,2500,2800,3200,3900,4200,4900,5700};
+	public static final int[] HARD_XP = {75,150,225,375,750,900,1100,1400,1600,1900,2400,3000,3400,3800,4300,4800,5900,6300,7300,8500};
+	public static final int[] DEADLY_XP = {100,200,400,500,1100,1400,1700,2100,2400,2800,3600,4500,5100,5700,6400,7200,8800,9500,10900,12700};
+	
+	private final int MAX_NUMBER_OF_MONSTERS_MULTIPLIER = 15;
+	private final float[] ENCOUTNER_MULTIPLIERS = {0.5f, 1.0f, 1.5f, 2.0f, 2.5f, 3.0f, 4.0f, 5.0f};
+	private RangedTreeMap<Integer> encounterSize;
+	
+	public EncounterXpCalculation() {
+		encounterSize = new RangedTreeMap<>();
+		encounterSize.put(0, 0);
+		encounterSize.put(1, 1);
+		encounterSize.put(2, 2);
+		encounterSize.put(3, 6, 3);
+		encounterSize.put(7, 10, 4);
+		encounterSize.put(11, 14, 5);
+		encounterSize.put(MAX_NUMBER_OF_MONSTERS_MULTIPLIER, 6);
+		encounterSize.put(MAX_NUMBER_OF_MONSTERS_MULTIPLIER+1, 7);
+	}
+	
+	public int calculateXP(ArrayList<Integer> partyLevel, int numberOfMonsters, int[] difficulty) {
+		int calculatedXP = 0;
+		for (Integer level : partyLevel) {
+			calculatedXP += difficulty[level.intValue()-1];
+		}
+		
+		int partySizeModifier = 0;
+		if (partyLevel.size() < 3) {
+			partySizeModifier = 1;
+		}
+		if (partyLevel.size() > 5) {
+			partySizeModifier = -1;
+		}
+
+		numberOfMonsters = Math.abs(numberOfMonsters);
+		numberOfMonsters = Math.min(numberOfMonsters, MAX_NUMBER_OF_MONSTERS_MULTIPLIER);
+		
+		float modifiedXP = (float) calculatedXP / ENCOUTNER_MULTIPLIERS[encounterSize.get(numberOfMonsters) + partySizeModifier];
+		calculatedXP = (int) modifiedXP;
+		
+		return calculatedXP;
+	}
+
+}
